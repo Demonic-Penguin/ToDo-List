74a0a91fcbbd1998dc402e0998205f53341f662a
Demonic Penguin <hild.brian@gmail.com>
2024-11-08 16:38:54 -0600

    Updated Sign up and Login

--- App.tsx
+++ App.tsx
@@ -1,40 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
 import React from "react";
-import { Button, View, StyleSheet, SafeAreaView } from "react-native";
+import { Button, View, StyleSheet, SafeAreaView, Text } from "react-native";
 
 import { Amplify } from "aws-amplify";
-import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
 
 import outputs from "./amplify_outputs.json";
 
+import {
+  Authenticator,
+  useAuthenticator,
+  useTheme,
+} from '@aws-amplify/ui-react-native';
+
+// import awsconfig from './aws-exports.json';
+
 Amplify.configure(outputs);
+// Amplify.configure(awsconfig);
 
-const SignOutButton = () => {
-  const { signOut } = useAuthenticator();
-
+const MyAppHeader = () => {
+  const {
+    tokens: { space, fontSizes },
+  } = useTheme();
   return (
-    <View style={styles.signOutButton}>
-      <Button title="Sign Out" onPress={signOut} />
+    <View>
+      <Text style={{ fontSize: fontSizes.xxxl, padding: space.xl }}>
+        ToDo
+      </Text>
     </View>
   );
 };
 
-const App = () => {
+function SignOutButton() {
+  const { signOut } = useAuthenticator();
+  return <Button onPress={signOut} title="Sign Out" />;
+}
+
+function App() {
+  const {
+    tokens: { colors },
+  } = useTheme();
+
   return (
     <Authenticator.Provider>
-      <Authenticator>
-        <SafeAreaView>
+      <Authenticator
+        // will wrap every subcomponent
+        Container={(props) => (
+          // reuse default `Container` and apply custom background
+          <Authenticator.Container
+            {...props}
+            style={{ backgroundColor: colors.pink[20] }}
+          />
+        )}
+        // will render on every subcomponent
+        Header={MyAppHeader}
+      >
+        <View style={style.container}>
           <SignOutButton />
-        </SafeAreaView>
+        </View>
       </Authenticator>
     </Authenticator.Provider>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  signOutButton: {
-    alignSelf: "flex-end",
-  },
+const style = StyleSheet.create({
+  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
 });
 
 export default App;
